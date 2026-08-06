63be9076041cb9911ffd84ba57b67be211e900c7
Stein-N <nico-stein-98@sbvl.de>
2026-08-06 08:24:42 +0200

    add fabric api and loom.mods discovery

--- src/main/kotlin/net/morthen/gradle/multiloader/plugins/FabricLoom.kt
+++ src/main/kotlin/net/morthen/gradle/multiloader/plugins/FabricLoom.kt
@@ -3,8 +3,11 @@ package net.morthen.gradle.multiloader.plugins
 import net.fabricmc.loom.api.LoomGradleExtensionAPI
 import net.morthen.gradle.multiloader.api.MultiloaderExtension
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.the
+import org.gradle.language.jvm.tasks.ProcessResources
 
 object FabricLoom {
     const val PLUGIN_ID = "net.fabricmc.fabric-loom"
@@ -16,7 +19,14 @@ fun applyFabricLoom(target: Project, ext: MultiloaderExtension) = with(target) {
     val loom = the<LoomGradleExtensionAPI>()
 
     dependencies {
-        "minecraft"("com.mojang:minecraft:${ext.minecraftVersion.get()}")
-        "implementation"("net.fabricmc:fabric-loader:${ext.fabricLoaderVersion.get()}")
+        "minecraft"("com.mojang:minecraft:${ ext.minecraftVersion.get() }")
+        "implementation"("net.fabricmc:fabric-loader:${ ext.fabricLoaderVersion.get() }")
+        "implementation"("net.fabricmc.fabric-api:fabric-api:${ ext.fabricApiVersion.get() }")
+    }
+
+    loom.mods {
+        create(ext.modId.get()) {
+            sourceSet("main")
+        }
     }
 }
